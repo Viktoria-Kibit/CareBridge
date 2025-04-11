d4c103df784cf7e3d378145113a046ef29ce8e9d
CloudyDO <olenadanchenko07@gmail.com>
2025-04-11 13:18:57 +0300

    feat: create AdCard component

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,10 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "../pages/Home/Home";
 import Header from "../components/Header/Header";
+import AdCard from "../components/AdCard/AdCard";
 
 const AppRoutes = () => (
   <BrowserRouter>
     <Header /> {/* Додаємо Header тут */}
+    <AdCard name="Боні" sex="female" age={3} rate = {5} visiting={20} /> {}
     <Routes>
       <Route path="/" element={<Home />} />
     </Routes>
