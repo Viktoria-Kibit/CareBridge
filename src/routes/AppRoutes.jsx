e234a79bfbb4b500d4640d9838783f1422f737ed
CloudyDO <olenadanchenko07@gmail.com>
2025-04-11 16:36:32 +0300

    trying to resolve merge conflicts

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -7,8 +7,6 @@ import Footer from "../components/Footer/Footer";
 const AppRoutes = () => (
   <BrowserRouter>
     <Header /> {/* Додаємо Header тут */}
-    {/* <AdCard name="Боні" sex="female" age={3} rate = {5} visiting={20} /> {} */}
-    {/* <Search /> */}
     <Routes>
       <Route path="/" element={<Home />} />
     </Routes>
