cf55c5aa91af0296af40097a0360a05892846bd5
CloudyDO <olenadanchenko07@gmail.com>
2025-04-11 16:33:25 +0300

    refactor: removed wrong lines
    I used those lines to test my code

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -3,14 +3,12 @@ import Home from "../pages/Home/Home";
 import Header from "../components/Header/Header";
 import Footer from "../components/Footer/Footer";
 
-import AdCard from "../components/AdCard/AdCard";
-import Search from "../components/Search/Search";
 
 const AppRoutes = () => (
   <BrowserRouter>
     <Header /> {/* Додаємо Header тут */}
-    <AdCard name="Боні" sex="female" age={3} rate = {5} visiting={20} /> {}
-    <Search />
+    {/* <AdCard name="Боні" sex="female" age={3} rate = {5} visiting={20} /> {} */}
+    {/* <Search /> */}
     <Routes>
       <Route path="/" element={<Home />} />
     </Routes>
