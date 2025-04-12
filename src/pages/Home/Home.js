c5e1ada9f5f1f7f72e93165b430f6eb01b4da5ec
CloudyDO <olenadanchenko07@gmail.com>
2025-04-12 10:53:01 +0300

    test: added carousel to home page
    added cards to home page and created array

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -8,6 +8,14 @@ import NewsSection from "../../components/HomePageComponents/NewsSection/NewsSec
 import FormSect from "../../components/AddAdForm/AddAdForm";
 import Carousel from "../../components/CardCarousel/CardCarousel";
 
+AdCard({})
+const cards = [
+  <AdCard name={"name1"} sex={"female"}  age={1} visiting={20} rate={3.1} />,
+  <AdCard name={"name2"} sex={"female"}  age={2} visiting={10} rate={3.5} />,
+  <AdCard name={"name3"} sex={"male"}  age={1} visiting={4} rate={1.0} />,
+  <AdCard name={"name4"} sex={"female"}  age={4} visiting={22} rate={5.0} />
+]
+
 const Home = () => {
   return (
     <div>
@@ -15,6 +23,7 @@ const Home = () => {
       <FeaturesSection />
       <AboutPlatform />
       <NewsSection />
+      <Carousel cards={cards}/>
     </div>
   );
 };
