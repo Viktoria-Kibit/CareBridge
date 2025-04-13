4f3b3ad84a09fe60d6364c559c4c1a6e6ed87863
CloudyDO <olenadanchenko07@gmail.com>
2025-04-13 13:27:34 +0300

    commented what i used for testing

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -7,24 +7,24 @@ import AboutPlatform from "../../components/HomePageComponents/AboutPlatform/Abo
 import NewsSection from "../../components/HomePageComponents/NewsSection/NewsSection";
 import ShelterProfile from "../ShelterProfile/ShelterProfile";
 import AdCard from "../../components/AdCard/AdCard";
-const shelter = {
-    name:"Лапки Надії",
-    rating: 4.5,
-    email: "Shelter@gmail.com",
-    phone: "+380970000",
-    savedAnimalsCount: 60,
-    currentAnimalsAtShelterCount: 20,
-    reviews : [
-      { author: "WOW",
-        comment: "Nice shelter" },
-      { author: "Mom",
-        comment:"I like this place" }
-    ],
-    donations: 9000,
-    ads: [
-      <AdCard type={"cat"} name={"Kitty"} age={2} sex={"female"} city={"Rivne"} rate={3} />
-    ]
-}
+// const shelter = {
+//     name:"Лапки Надії",
+//     rating: 4.5,
+//     email: "Shelter@gmail.com",
+//     phone: "+380970000",
+//     savedAnimalsCount: 60,
+//     currentAnimalsAtShelterCount: 20,
+//     reviews : [
+//       { author: "WOW",
+//         comment: "Nice shelter" },
+//       { author: "Mom",
+//         comment:"I like this place" }
+//     ],
+//     donations: 9000,
+//     ads: [
+//       <AdCard type={"cat"} name={"Kitty"} age={2} sex={"female"} city={"Rivne"} rate={3} />
+//     ]
+// }
 const Home = () => {
   return (
     <div>
@@ -32,7 +32,6 @@ const Home = () => {
       <FeaturesSection />
       <AboutPlatform />
       <NewsSection />
-      <ShelterProfile shelter={shelter}/>
     </div>
   );
 };
